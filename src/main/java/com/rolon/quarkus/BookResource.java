d63986d0b742549f3f70ddb29d415106aca190d9
JesusRRr <rolonjesus1996@gmail.com>
2021-02-17 17:41:01 -0600

    fix: db properties fixed

--- src/main/java/com/rolon/quarkus/BookResource.java
+++ src/main/java/com/rolon/quarkus/BookResource.java
@@ -1,16 +1,24 @@
 package com.rolon.quarkus;
 
+import com.rolon.quarkus.data.Book;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 @Path("/book")
 public class BookResource {
+    @Inject
+    EntityManager entityManager;
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    @Produces(MediaType.APPLICATION_JSON)
+
+    public List<Book> hello() {
+        return entityManager.createQuery("Select b from Book b", Book.class).getResultList();
     }
 }
