9aec9c31e1296f0362c7cf7aeecff9e8d2a82af4
JesusRRr <rolonjesus1996@gmail.com>
2021-02-18 13:19:04 -0600

    chore: AuthorService created

---
+++ src/main/java/com/rolon/quarkus/service/AuthorService.java
@@ -0,0 +1,38 @@
+package com.rolon.quarkus.service;
+
+import com.rolon.quarkus.data.Author;
+import com.rolon.quarkus.repository.AuthorRepository;
+
+import javax.inject.Inject;
+import java.util.List;
+
+public class AuthorService implements IAuthorService{
+    @Inject
+    AuthorRepository authorRepository;
+    @Override
+    public List<Author> findAll() {
+        return (List<Author>) authorRepository.findAll();
+    }
+
+    @Override
+    public Author findByID(Integer id) {
+        return authorRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Author deleteByID(Integer id) {
+        Author author = findByID(id);
+        return author;
+    }
+
+    @Override
+    public Author updateByID(Integer id, Author author) {
+        deleteByID(id);
+        return save(author);
+    }
+
+    @Override
+    public Author save(Author author) {
+        return authorRepository.save(author);
+    }
+}
