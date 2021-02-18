eba29e70ef31f821a0ef36fd2b9adc897e604791
JesusRRr <rolonjesus1996@gmail.com>
2021-02-18 14:00:16 -0600

    made the relation between Books and Author

--- src/main/java/com/rolon/quarkus/AuthorController.java
+++ src/main/java/com/rolon/quarkus/AuthorController.java
@@ -5,9 +5,7 @@ import com.rolon.quarkus.service.AuthorService;
 import javafx.application.Application;
 
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
@@ -19,6 +17,13 @@ public class AuthorController {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<Author> listAuthors(){
+        return authorService.findAll();
+    }
 
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Author saveAuthor(Author author){
+        return authorService.save(author);
     }
 }
