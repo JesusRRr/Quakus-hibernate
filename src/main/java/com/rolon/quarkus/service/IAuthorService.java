c7d367f1b0bffb453fcef5eaeb8b776084c25a00
JesusRRr <rolonjesus1996@gmail.com>
2021-02-18 13:12:36 -0600

    Chore: Declaring Methods in IAuthorService

--- src/main/java/com/rolon/quarkus/service/IAuthorService.java
+++ src/main/java/com/rolon/quarkus/service/IAuthorService.java
@@ -1,2 +1,13 @@
-package com.rolon.quarkus.service;public class IAuthorService {
+package com.rolon.quarkus.service;
+
+import com.rolon.quarkus.data.Author;
+
+import java.util.List;
+
+public interface IAuthorService {
+    public List<Author> findAll();
+    public Author findByID(Integer id);
+    public Author deleteByID(Integer id);
+    public Author updateByID(Integer id, Author author);
+    public Author save(Author author);
 }
